02f56138e4fa24846d587ba3fe0f88334756d7d2
tfolly <tfolly@student.42.fr>
2016-01-13 16:15:17 +0100

    norme strndup

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tfolly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 11:42:32 by tfolly            #+#    #+#             */
-/*   Updated: 2016/01/05 17:44:04 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/01/10 12:37:02 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,4 +89,5 @@ int					ft_lstfold(t_list *lst, int (*f)(void *, void *));
 void				ft_lstdelcmp(t_list **begin, void (*del)(void *, size_t),
 						int(cmp)(void *));
 char				*ft_strndup(const char *s1, size_t n);
+char				*ft_strend(char *str);
 #endif
