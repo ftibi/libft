b88900df119829b1a2586bfd58b08b79f9bd9cd8
tfolly <tfolly@student.42.fr>
2016-01-22 18:44:13 +0100

    uitoa

---
+++ ft_uitoa_base.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_uitoa_base.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfolly <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/21 17:59:34 by tfolly            #+#    #+#             */
+/*   Updated: 2016/01/22 18:43:36 by tfolly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char		*ft_init_itoa(unsigned int nb, unsigned int base,
+		unsigned int *pow, int signe)
+{
+	char	*ret;
+
+	*pow = 1;
+	while (nb >= base)
+	{
+		nb /= base;
+		(*pow)++;
+	}
+	if (!(ret = ft_strnew(*pow + (signe == -1))))
+		return (NULL);
+	*pow = ft_pow(base, *pow - 1);
+	return (ret);
+}
+
+static char		*ft_signe_itoa(int signe, char *ret)
+{
+	if (signe == -1)
+	{
+		*ret = '-';
+		ret++;
+	}
+	return (ret);
+}
+
+static char		ft_char_itoa(unsigned int pow, int nb)
+{
+	char	ret;
+
+	ret = nb / pow;
+	if (ret >= 10)
+		ret += 'a' - 10;
+	else
+		ret += '0';
+	return (ret);
+}
+
+char			*ft_uitoa_base(unsigned int nb, unsigned int base)
+{
+	char			*ret;
+	char			*save;
+	int				signe;
+	unsigned int	pow;
+
+	if (!(ret = ft_init_itoa(nb, base, &pow, signe)))
+		return (NULL);
+	save = ret;
+	ret = ft_signe_itoa(signe, ret);
+	while (pow > 0)
+	{
+		*ret = ft_char_itoa(pow, nb);
+		nb = nb % pow;
+		pow = pow / base;
+		ret++;
+	}
+	return (save);
+}
